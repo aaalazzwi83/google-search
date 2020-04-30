682e070034b28e1a1d00b1f94e841bcb80f802f7
Ammar Alazzawi <aaalazzawi83@gmail.com>
2020-04-29 22:03:28 -0700

    test

---
+++ client/build/precache-manifest.cab6daf668bc2c60fd9793dc7dabb788.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "887dbe36e014f5f162cd",
+    "url": "/static/css/main.5450d8b4.chunk.css"
+  },
+  {
+    "revision": "887dbe36e014f5f162cd",
+    "url": "/static/js/main.cc8dfe6c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cb17bcf018bf7c55997e",
+    "url": "/static/js/2.09d13a7d.chunk.js"
+  },
+  {
+    "revision": "d4a1962849a8b9a67817e070ab965d7b",
+    "url": "/index.html"
+  }
+];
